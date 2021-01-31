25a599970b6e6dc859dc12bb980e4fb54865e514
UnravelSports <joris.bekkers@gmail.com>
2021-01-31 17:00:03 +0100

    init

---
+++ android/app/src/main/kotlin/com/unravelsports/base_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.unravelsports.base_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+// TODO: need to add internet permissions
+// https://stackoverflow.com/questions/64197752/bad-state-insecure-http-is-not-allowed-by-platform
+
+// TODO: implement stream.listen instead of StreamBuilder, because the latter skips values?
+// https://stackoverflow.com/questions/54169848/streambuilder-not-receiving-some-snapshot-data?rq=1
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  WebSocketChannel _channel;
+
+  HashMap<String, dynamic> _steamData;
+
+  @override
+  void initState() {
+    super.initState();
+    _channel = IOWebSocketChannel.connect(
+      'ws://142.93.238.122:8000/ws-3bebdd64-4f6f-4c62-a50b-5271bb06084c',
+    );
+  }
+
+  @override // need this to close if we kill the homepage
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebSocket'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: StreamBuilder(
+            stream: _channel.stream,
+            initialData: HashMap<String, dynamic>(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasError) {
+                return Container(width: 0.0, height: 0.0);
+              } else if (!snapshot.hasData) {
+                return Container(width: 0.0, height: 0.0);
+//              } else if (snapshot.data.isEmpty()) {
+//                return null;
+              } else {
+                try {
+                  _steamData = new HashMap<String, dynamic>.from(
+                    json.decode(snapshot.data),
+                  );
+                  return CustomPaint(
+                    painter: DotPainter(
+                      playerCoords: _steamData['players'],
+                    ),
+                  );
+                } catch (e) {
+                  return Container(width: 0.0, height: 0.0);
+                }
+              }
+            },
+          ),
+        ),
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              color: Colors.blueAccent,
+              child: Text('Play'),
+              onPressed: () {},
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              color: Colors.redAccent,
+              child: Text('Pause'),
+              onPressed: () {
+                _channel.sink.close();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DotPainter extends CustomPainter {
+  final List<dynamic> playerCoords;
+
+  DotPainter({this.playerCoords});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pointMode = ui.PointMode.points;
+    // convert list of list to list of Offset()'s
+    final points = playerCoords.map((dynamic player) {
+      player = new List<dynamic>.from(player);
+      return Offset(player[0], player[1]);
+    }).toList();
+
+    print(points);
+    final spotPaint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 5
+      ..strokeCap = StrokeCap.round;
+    canvas.drawPoints(pointMode, points, spotPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw true;
+  }
+}
